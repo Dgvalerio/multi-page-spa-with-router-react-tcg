b853921ff5aef826b027ead5f291845efc06ff3b
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-21 12:06:02 -0300

    Class 283 - Preventing Possibly Unwanted Route Transitions with the "Prompt" Component

--- src/components/quotes/QuoteForm.tsx
+++ src/components/quotes/QuoteForm.tsx
@@ -1,4 +1,5 @@
-import React, { FormEvent, useRef } from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import { IQuote } from '../../interfaces';
 import Card from '../UI/Card';
@@ -12,6 +13,8 @@ const QuoteForm = ({
   onAddQuote: ({ author, text }: IQuote) => void;
   isLoading: boolean;
 }): JSX.Element => {
+  const [isEntered, setIsEntered] = useState(false);
+
   const authorInputRef = useRef<HTMLInputElement>(null);
   const textInputRef = useRef<HTMLTextAreaElement>(null);
 
@@ -26,30 +29,50 @@ const QuoteForm = ({
     onAddQuote({ author: enteredAuthor, text: enteredText, id: '?' });
   };
 
-  return (
-    <Card>
-      <form className={classes.form} onSubmit={submitFormHandler}>
-        {isLoading && (
-          <div className={classes.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => setIsEntered(false);
 
-        <div className={classes.control}>
-          <label htmlFor="author">Author</label>
-          <input type="text" id="author" ref={authorInputRef} />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="text">Text</label>
-          <textarea id="text" rows={5} ref={textInputRef} />
-        </div>
-        <div className={classes.actions}>
-          <button type="submit" className="btn">
-            Add Quote
-          </button>
-        </div>
-      </form>
-    </Card>
+  const formFocusedHandler = () => setIsEntered(true);
+
+  return (
+    <>
+      <Prompt
+        when={isEntered}
+        message={() =>
+          'Are you sure want to leave? All your entered data will be lost!'
+        }
+      />
+      <Card>
+        <form
+          onFocus={formFocusedHandler}
+          className={classes.form}
+          onSubmit={submitFormHandler}
+        >
+          {isLoading && (
+            <div className={classes.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={classes.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows={5} ref={textInputRef} />
+          </div>
+          <div className={classes.actions}>
+            <button
+              type="submit"
+              className="btn"
+              onClick={finishEnteringHandler}
+            >
+              Add Quote
+            </button>
+          </div>
+        </form>
+      </Card>
+    </>
   );
 };
 
