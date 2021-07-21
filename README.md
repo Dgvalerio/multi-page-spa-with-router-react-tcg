284997d47776defc8dd90700d5b4c34e26fb910a
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-21 08:40:38 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projeto feito durante as aulas do curso "React - The Complete Guide".
+## Seção Seção 20: Building a Multi-Page SPA with React Router
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+const App: FC = () => (
+  <div>
+    <h2>Let&apos;s get started!</h2>
+  </div>
+);
+
+export default App;
