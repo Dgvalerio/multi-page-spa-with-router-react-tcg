d94cc02f365ef6343f4a6f05aa3fef9e11e4df51
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-21 09:17:17 -0300

    Class 273 - Working with Nested Routes

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,13 @@
 import React, { FC } from 'react';
+import { Route } from 'react-router-dom';
 
-const Welcome: FC = () => <h1>The Welcome Page</h1>;
+const Welcome: FC = () => (
+  <section>
+    <h1>The Welcome Page</h1>
+    <Route path="/welcome/new-user">
+      <p>Welcome, new user!</p>
+    </Route>
+  </section>
+);
 
 export default Welcome;
